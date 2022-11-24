51ececa40644f946a7b3799927282c8609088054
MichaelAkin02 <118995270+MichaelAkin02@users.noreply.github.com>
2022-11-24 15:48:06 +0000

    Update helloWorld.c

--- helloWorld.c
+++ helloWorld.c
@@ -1,14 +1,7 @@
-#include<stdio.h> 
-main()
-{    
-   printf("Hello World\n");
-}
-
-Save, add and commit helloWorld.c with an appropriate message.
-Now modify the program so it is reads
 #include<stdio.h>  
 main() 
 {     
    printf("Hello World\n");
    printf("Hello You!\n");
 }
+
